65f570b21fe46bc648d60bae8cbf31e4d9944b5a
Simon Opelt <simon@software-architects.at>
2014-02-13 23:07:33 +0100

    cleanup
    whitespace, JShint

--- ZendeskChatNotifications.user.js
+++ ZendeskChatNotifications.user.js
@@ -8,81 +8,81 @@
 // @copyright   2014 software architects gmbh
 // ==/UserScript==
 
-var currentNotifications = new Array();
+var currentNotifications = [];
 var chatsDiv = null;
 
 function processChatsAdded(event) {
-    // find invite nodes
-    if (Notification.permission === "granted") {
-        var invites = chatsDiv.find('.ember-view.invite');
-        $.each(invites, function( index, value ) {
-            if ($.inArray(value, currentNotifications) < 0) {
-                var username = chatsDiv.find('a.username').text();
-                var subject = chatsDiv.find('div.chat-subject').text();
-                if (username && subject) {
+	// find invite nodes
+	if (Notification.permission === "granted") {
+		var invites = chatsDiv.find('.ember-view.invite');
+		$.each(invites, function( index, value ) {
+			if ($.inArray(value, currentNotifications) < 0) {
+				var username = chatsDiv.find('a.username').text();
+				var subject = chatsDiv.find('div.chat-subject').text();
+				if (username && subject) {
 					console.debug('ZendeskChat: new chat request found - notifying');
-                    var title = "Chat request from: " + username;
-                    var options = {
-                        body : subject,
-                        icon : 'http://www.zendesk.com/favicon.ico'
-                    };
-                    
-                    var notification = new Notification(title, options);
-                    notification.onclick = function(x) { window.focus(); };
-                    currentNotifications.push(value);
-                    $(value).on("remove", function () {
-                        console.debug('ZendeskChat: chat request ended - removing notification');
-                        currentNotifications.splice( $.inArray(this, currentNotifications), 1 );
-                        notification.close();
-                	});
-                }
-            }
-        });
-    }
+					var title = "Chat request from: " + username;
+					var options = {
+						body : subject,
+						icon : 'http://www.zendesk.com/favicon.ico'
+					};
+					
+					var notification = new Notification(title, options);
+					notification.onclick = function(x) { window.focus(); };
+					currentNotifications.push(value);
+					$(value).on("remove", function () {
+						console.debug('ZendeskChat: chat request ended - removing notification');
+						currentNotifications.splice( $.inArray(this, currentNotifications), 1 );
+						notification.close();
+					});
+				}
+			}
+		});
+	}
 }
 
 // try to find the main chat dom element
 function findChat() {
-    chatsDiv = $('#incoming-chats');
-    if (chatsDiv.length == 0) {
-        // not found -> DOM not fully built -> retry in due time
-        window.setTimeout(function () { findChat()}, 1000);
-    }
-    else {
+	chatsDiv = $('#incoming-chats');
+	if (chatsDiv.length === 0) {
+		// not found -> DOM not fully built -> retry in due time
+		window.setTimeout(function () { findChat(); }, 1000);
+	}
+	else {
 		console.debug('ZendeskChat: found incoming-chats');
-        // if the current div gets lost -> look for it again
-        chatsDiv.on("remove", function () {
+		// if the current div gets lost -> look for it again
+		chatsDiv.on("remove", function () {
 			console.debug('ZendeskChat: incoming-chats lost - rediscovering');
-            findChat();
-        });
-        
-        chatsDiv.on('DOMNodeInserted', processChatsAdded);
-        
-        // if permissions were not yet granted provide button
-        if (Notification.permission !== "granted" && Notification.permission !== 'denied') {
+			findChat();
+		});
+		
+		chatsDiv.on('DOMNodeInserted', processChatsAdded);
+		
+		// if permissions were not yet granted provide button
+		if (Notification.permission !== "granted" && Notification.permission !== 'denied') {
 			console.debug('ZendeskChat: notification permission not yet granted - offering request');
 
-            var button = $('<a class="attachment">enable notifications</a>');
-            $('#chat-header').append(button);
-            button.click(function() {
+			var button = $('<a class="attachment">enable notifications</a>');
+			$('#chat-header').append(button);
+			button.click(function() {
 				console.debug('ZendeskChat: requesting notification permission');
-                Notification.requestPermission(function (permission) {
+				Notification.requestPermission(function (permission) {
 					console.debug('ZendeskChat: notification permission request handled: ' + permission);
-            		if(!('permission' in Notification)) {
-                		Notification.permission = permission;
-            		}
-                    
-                    button.remove();
-                });
-            });
-        }
-    }
+					if(!('permission' in Notification)) {
+						Notification.permission = permission;
+					}
+					
+					button.remove();
+				});
+			});
+		}
+	}
 }
 
 // check for notification support
 if (("Notification" in window)) {
 	console.debug('ZendeskChat: notifications supported');
-	window.setTimeout(function () { findChat()}, 500);
+	window.setTimeout(function () { findChat(); }, 500);
 }
 else {
 	console.debug('ZendeskChat: notifications not supported');
