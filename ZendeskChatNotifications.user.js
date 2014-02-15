ebbbd8ed1a6c752aea0b614efbd4a020727434f9
Simon Opelt <simon@software-architects.at>
2014-02-15 12:35:53 +0100

    added first sound looping
    * whitespace => spaces
    * config checkboxes

--- ZendeskChatNotifications.user.js
+++ ZendeskChatNotifications.user.js
@@ -3,87 +3,127 @@
 // @namespace   http://www.software-architects.at
 // @description Adds desktop notifications to zendesk chat.
 // @match       https://*.zendesk.com/agent/*
-// @grant       none
-// @version     1.1
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @version     1.2
 // @copyright   2014 software architects gmbh
 // ==/UserScript==
 
 var currentNotifications = [];
 var chatsDiv = null;
+var loopTimer = null;
+var enableDesktop = false;
+var enableLoop = false;
 
 function processChatsAdded(event) {
-	// find invite nodes
-	if (Notification.permission === "granted") {
-		var invites = chatsDiv.find('.ember-view.invite');
-		$.each(invites, function( index, value ) {
-			if ($.inArray(value, currentNotifications) < 0) {
-				var username = chatsDiv.find('a.username').text();
-				var subject = chatsDiv.find('div.chat-subject').text();
-				if (username && subject) {
-					console.debug('ZendeskChat: new chat request found - notifying');
-					var title = "Chat request from: " + username;
-					var options = {
-						body : subject,
-						icon : 'http://www.zendesk.com/favicon.ico'
-					};
-					
-					var notification = new Notification(title, options);
-					notification.onclick = function(x) { window.focus(); };
-					currentNotifications.push(value);
-					$(value).on("remove", function () {
-						console.debug('ZendeskChat: chat request ended - removing notification');
-						currentNotifications.splice( $.inArray(this, currentNotifications), 1 );
-						notification.close();
-					});
-				}
-			}
-		});
-	}
+    if (enableDesktop && Notification.permission === "granted") {
+        // find invite nodes
+        var invites = chatsDiv.find('.ember-view.invite');
+        $.each(invites, function( index, value ) {
+        
+            // only handle each request once
+            if ($.inArray(value, currentNotifications) < 0) {
+                var username = chatsDiv.find('a.username').text();
+                var subject = chatsDiv.find('div.chat-subject').text();
+                
+                // only if we could gather all the info
+                if (username && subject) {
+                    console.debug('ZendeskChat: new chat request - notifying');
+                    var title = "Chat request: " + username;
+                    var options = {
+                        body : subject,
+                        icon : 'http://www.zendesk.com/favicon.ico'
+                    };
+                    
+                    var notification = new Notification(title, options);
+                    notification.onclick = function(x) { window.focus(); };
+                    currentNotifications.push(value);
+                    
+                    // sound looping
+                    if (enableLoop && loopTimer === null) {
+                        console.debug('ZendeskChat: looping sound');
+                        loopTimer = window.setInterval(function () {
+                            soundManager.sounds.invite.play();
+                            console.debug('ZendeskChat: beeping');
+                        }, 4000);
+                    }
+                    
+                    // when the zendesk-internal notification closes
+                    $(value).on("remove", function () {
+                        console.debug('ZendeskChat: chat request ended');
+                        currentNotifications.splice( $.inArray(this, currentNotifications), 1 );
+                        if (currentNotifications.length === 0) {
+                            console.debug('ZendeskChat: ending sound loop');
+                            window.clearInterval(loopTimer);
+                            loopTimer = null;
+                        }
+                        
+                        notification.close();
+                    });
+                }
+            }
+        });
+    }
 }
 
 // try to find the main chat dom element
 function findChat() {
-	chatsDiv = $('#incoming-chats');
-	if (chatsDiv.length === 0) {
-		// not found -> DOM not fully built -> retry in due time
-		window.setTimeout(function () { findChat(); }, 1000);
-	}
-	else {
-		console.debug('ZendeskChat: found incoming-chats');
-		// if the current div gets lost -> look for it again
-		chatsDiv.on("remove", function () {
-			console.debug('ZendeskChat: incoming-chats lost - rediscovering');
-			findChat();
-		});
-		
-		chatsDiv.on('DOMNodeInserted', processChatsAdded);
-		
-		// if permissions were not yet granted provide button
-		if (Notification.permission !== "granted" && Notification.permission !== 'denied') {
-			console.debug('ZendeskChat: notification permission not yet granted - offering request');
-
-			var button = $('<a class="attachment">enable notifications</a>');
-			$('#chat-header').append(button);
-			button.click(function() {
-				console.debug('ZendeskChat: requesting notification permission');
-				Notification.requestPermission(function (permission) {
-					console.debug('ZendeskChat: notification permission request handled: ' + permission);
-					if(!('permission' in Notification)) {
-						Notification.permission = permission;
-					}
-					
-					button.remove();
-				});
-			});
-		}
-	}
+    chatsDiv = $('#incoming-chats');
+    if (chatsDiv.length === 0) {
+        // not found -> DOM not fully built -> retry in due time
+        window.setTimeout(function () { findChat(); }, 1000);
+    }
+    else {
+        console.debug('ZendeskChat: found incoming-chats');
+        // if the current div gets lost -> look for it again
+        chatsDiv.on("remove", function () {
+            console.debug('ZendeskChat: incoming-chats lost - rediscovering');
+            findChat();
+        });
+        
+        chatsDiv.on('DOMNodeInserted', processChatsAdded);
+        
+        // for pre-1.2 scripts assume that a granted permission means notify
+        if (typeof GM_getValue('desktop') === 'undefined' && Notification.permission === "granted") {
+            GM_setValue('desktop', true);
+        }
+        
+        // add notify checkbox
+        var notifications = $('<input type="checkbox">notify</input>');
+        enableDesktop = GM_getValue('desktop', false);
+        notifications.prop('checked', enableDesktop);
+        $('#chat-header').append(notifications);
+        notifications.change(function() {
+            enableDesktop = $(this).is(':checked');
+            GM_setValue('desktop', enableDesktop);
+            if (enableDesktop && Notification.permission !== "granted" && Notification.permission !== 'denied') {
+                console.debug('ZendeskChat: requesting notification permission');
+                Notification.requestPermission(function (permission) {
+                    console.debug('ZendeskChat: permission: ' + permission);
+                    if(!('permission' in Notification)) {
+                        Notification.permission = permission;
+                    }
+                });
+            }
+        });
+        
+        // add loop checkbox
+        var loop = $('<input type="checkbox">loop</input>');
+        enableLoop = GM_getValue('loop', false);
+        loop.prop('checked', enableLoop);
+        $('#chat-header').append(loop);
+        loop.change(function() {
+            enableLoop = $(this).is(':checked');
+            GM_setValue('loop', enableLoop);
+        });
+    }
 }
 
 // check for notification support
 if (("Notification" in window)) {
-	console.debug('ZendeskChat: notifications supported');
-	window.setTimeout(function () { findChat(); }, 500);
+    console.debug('ZendeskChat: notifications supported');
+    window.setTimeout(function () { findChat(); }, 500);
 }
 else {
-	console.debug('ZendeskChat: notifications not supported');
+    console.debug('ZendeskChat: notifications not supported');
 }
